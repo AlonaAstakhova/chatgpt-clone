adc9e52f477f431ce267194b7138fe4f63485c76
AlonaAstakhova <alyonasta@gmail.com>
2023-12-11 13:23:46 -0800

    fiture: show chatGPT chat and added dotenv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,108 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  const [value, setValue] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+
+  const createNewChat = () => {
+    setMessage(null)
+    setValue('')
+    setCurrentTitle(null)
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setMessage(null)
+    setValue('')
+    setCurrentTitle(uniqueTitle)
+  }
+
+  const getMassages = async () => {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        {
+          title: currentTitle,
+          role: 'user',
+          content: value,
+        },
+        { title: currentTitle, role: message.role, content: message.content },
+      ])
+    }
+    // eslint-disable-next-line
+  }, [message, currentTitle])
+
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
+  )
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  )
+  console.log(uniqueTitles)
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
-        <ul className="history"></ul>
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
+        </ul>
         <nav>
           <p> Made by Alona</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        {!currentTitle && <h1>GhatGPT clone</h1>}
+        <ul clasname="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMassages}>
+              ➢
+            </div>
+          </div>
+          <p className="info">
+            ChatGPT can make mistakes. Consider checking important information.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
